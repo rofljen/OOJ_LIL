aa7902b8e655cbbafdf1fe1124883bce927a290b
rofljen <rofljen@gmail.com>
2024-05-21 22:17:09 -0700

    Add files via upload

---
+++ definition_start.py
@@ -0,0 +1,17 @@
+# Python Object Oriented Programming by Joe Marini course example
+# Basic class definitions
+
+
+# TODO: create a basic class
+class Book:
+    def __init__(self, title):
+        self.title = title
+
+# TODO: create instances of the class
+book1 = Book("Brave New World")
+book2 = Book("War and Peace")
+
+# TODO: print the class and property
+print(book1)
+print(book1.title)
+
